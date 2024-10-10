05ef36164a0a4ce9a4a88dbdd8c3dd08a27042cb
Sunil Kumar Pandit <79739267+Sunil-Kumar-Pandit@users.noreply.github.com>
2024-10-10 11:30:28 +0530

    Update NavBar.tsx

--- src/app/NavBar.tsx
+++ src/app/NavBar.tsx
@@ -15,7 +15,7 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#">Home</Nav.Link>
+            <Nav.Link >Home</Nav.Link>
             {/* <Nav.Link href="/">Link</Nav.Link> */}
             {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="/">Action</NavDropdown.Item>
